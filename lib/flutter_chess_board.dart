25d57820bdae98670d729256c1c194dd9e48f9b6
Tomasz Bogun <tbogun72@gmail.com>
2025-01-12 21:52:08 +0000

    export static chessboard

--- lib/flutter_chess_board.dart
+++ lib/flutter_chess_board.dart
@@ -1,5 +1,6 @@
 library chess_board;
 export 'src/chess_board.dart';
+export 'src/static_chess_board.dart';
 export 'src/chess_board_controller.dart';
 export 'package:chess_vectors_flutter/chess_vectors_flutter.dart' hide State;
 export 'src/constants.dart';
