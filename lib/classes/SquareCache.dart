fe275ba8efff3cd7b6819ea02e994d548cbf975e
Tomasz Bogun <tbogun72@gmail.com>
2025-01-24 17:30:59 +0000

    Fix square cache
    the Colors toString() method works weird in production due to some optimisations, obects idk idk

--- lib/classes/SquareCache.dart
+++ lib/classes/SquareCache.dart
@@ -4,7 +4,7 @@ class SquareCache {
   static final Map<String, Widget> _cache = {};
 
   static Widget getSquare(bool isLightSquare, Color lightColor, Color darkColor) {
-    final key = isLightSquare ? "light_$lightColor" : "dark_$darkColor";
+    final key = isLightSquare ? "${lightColor.r}_${lightColor.g}_${lightColor.b}}" : "${darkColor.r}_${darkColor.g}_${darkColor.b}";
     if (!_cache.containsKey(key)) {
       _cache[key] = Container(
         color: isLightSquare ? lightColor : darkColor,
