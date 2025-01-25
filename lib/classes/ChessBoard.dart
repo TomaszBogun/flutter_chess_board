99b5075dc59a516853b98aaa81481f49be60a037
Tomasz Bogun <tbogun72@gmail.com>
2025-01-25 12:43:03 +0000

    Fix to board letters and colors

--- lib/classes/ChessBoard.dart
+++ lib/classes/ChessBoard.dart
@@ -231,8 +231,8 @@ class _ChessBoardState extends State<ChessBoard> {
     final List<String> ranks = widget.boardOrientation == PlayerColor.white ? ["8", "7", "6", "5", "4", "3", "2", "1"] : ["1", "2", "3", "4", "5", "6", "7", "8"];
 
     final double squareSize = widget.size! / 8;
-    final ui.Color textColor = boardColorToHexColor[widget.boardColor]![widget.boardOrientation == PlayerColor.white ? 0 : 1];
-    final ui.Color altTextColor = boardColorToHexColor[widget.boardColor]![widget.boardOrientation == PlayerColor.white ? 1 : 0];
+    final ui.Color textColor = boardColorToHexColor[widget.boardColor]![0];
+    final ui.Color altTextColor = boardColorToHexColor[widget.boardColor]![1];
 
     return Container(
       width: widget.size!,
